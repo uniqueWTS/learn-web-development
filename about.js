16c52b1efe480731a91ea01620618c4c0d77cb3a
uniqueWTS <710029721@qq.com>
2018-08-17 19:24:42 +0800

    add about page

---
+++ about.js
@@ -0,0 +1,3 @@
+/*about.css require index.css*/
+import './public/index.css';
+import './public/about.css';
--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,32 @@ module.exports = {
 	// 开发模式
 	mode: 'development',
 	// 入口文件
-	entry: './index.js',
+	// entry: './index.js',
+	entry: {
+		home: './index.js',
+		about:'./about.js'
+	},
 	// 输出目录和输出文件名称
 	output: {
 		path: path.resolve(__dirname, 'site'),
-		filename: '[name].bundle.js'
+		filename: 'public/[name].bundle.js'
 	},
 	// 插件
 	plugins: [
-		new HTMLWebpackPlugin({ template : './index.html' })
+		new HTMLWebpackPlugin({
+			title: "Home",
+			filename: "index.html",
+			template: "public/index.html",
+			inject: "head",
+			chunks: ['home']
+		}),
+		new HTMLWebpackPlugin({ 
+			title: "About",
+			filename: "about.html",
+			template: "public/about.html",
+			inject: "body",
+			chunks: ['about']
+		})
 	],
 	// 模块转换
 	module: {
